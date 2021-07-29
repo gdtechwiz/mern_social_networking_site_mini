a474ad49d86aaeeb34c4e043cbcf18e9a8ef9000
Gangadharan Nadar <gangadharan.nadar@yahoo.com>
2021-07-29 19:13:05 +0530

    A small social media sites like a scrapbook based on MERN stack

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import dotnev from 'dotenv';
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotnev.config();
 
 
 app.use(bodyParser.json({ limit : "30mb", extended:true }))
@@ -14,10 +14,13 @@ app.use(bodyParser.urlencoded({ limit : "30mb", extended:true }))
 app.use(cors());
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://root:root@123@cluster0.lbxhp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-const PORT = process.env.port || 5000;
+app.get('/', (req,res) => {
+    res.send("Hello Memories API");
+});
+ 
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL,{ useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)) )
     .catch((error) =>  console.log(error.message));
 
